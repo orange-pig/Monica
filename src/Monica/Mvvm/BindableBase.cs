9a2dc6b9ca1585d177f11f68e2ad9d9e338ed1b6
standevil <standevil@gmail.com>
2020-12-16 20:02:33 +0800

    add BindableBase supported Caller Info Attributes, fix once change notify all.

--- src/Monica/Mvvm/BindableBase.cs
+++ src/Monica/Mvvm/BindableBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace Monica.Mvvm
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected bool SetProperty<T>(ref T storage, T value, String propertyName = null)
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] String propertyName = null)
         {
             if (Equals(storage, value))
             {
